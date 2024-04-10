e29c84de02bcf25db9cd2330809b9a69204b9686
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2024-04-09 23:06:17 -0400

    Layout of landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head"
 import { Inter } from "next/font/google"
 import Nav from "./components/Nav"
+import Image from "next/image"
+import landingImage from "../public/samuel-regan-asante-full.jpg"
 // import bg-popcorn-bw from '../public/bg-movies-bw.png'
 
 // const inter = Inter({ subsets: ["latin"] })
@@ -8,18 +10,46 @@ import Nav from "./components/Nav"
 export default function Home() {
   //TODO: Use padding-top 36 to ensure header doesn't overlap
   return (
-    <>
-      <div className="flex pr-32 justify-center lg:bg-[url('../public/samuel-regan-asante-full.jpg')] bg-right h-svh bg-no-repeat bg-blend-screen bg-blue-950">
-        <h1 className="flex 2xl:text-6xl text-2xl font-bold text-white pt-72 max-w-2xl pr-0">
-          The App for Film Buffs.
-        </h1>
-        <div className="text-white">
-          <span>Track movies you’ve watched.</span>
-          <span>Rate and review.</span>
-          <span>Find new favorites.</span>
-          <span>All in one app.</span>
+    //hero
+
+    <section className=" bg-slate-950 ">
+      <div className="flex flex-row justify-between container h-svh w-full bg-slate-950 mx-auto pt-24">
+        <div className="flex items-center  gap-8 flex-col text-white mx-auto pt-48">
+          <h1 className="text-2xl md:text-3xl lg:text-5xl font-bold">
+            The Must-See App for Film Buffs.
+          </h1>
+          <div className="text-center text-lg">
+            <p>Log movies you watch.</p>
+            <p>Rate and review.</p>
+            <p>Find new favorites.</p>
+            <p>All in one place.</p>
+          </div>
+          <div className="flex gap-10">
+            <button
+              type="button"
+              className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 rounded-lg text-md px-5 py-2.5 text-center me-2 mb-2"
+            >
+              Learn More
+            </button>
+            <button
+              type="button"
+              className="text-red-600 bg-transparent focus:ring-4 focus:outline-none border-4 focus:ring-white border-red-600 font-medium rounded-lg text-md px-5 py-2.5 text-center me-2 mb-2"
+            >
+              Get Started
+            </button>
+          </div>
+        </div>
+
+        <div className=" justify-self-end overflow-hidden ">
+          <Image
+            className="hidden xl:flex "
+            src={landingImage}
+            width={640}
+            height={853}
+            alt="landing image"
+          />
         </div>
       </div>
-    </>
+    </section>
   )
 }

2eb26c9b9971373970f3d1535d903fba7abeb6cd
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2024-04-10 14:14:16 -0400

    Landing page layout complete. TODO: Functionality.

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,10 @@ export default function Home() {
   return (
     //hero
 
-    <section className=" bg-slate-950 ">
+    <section className="bg-slate-950 ">
       <div className="flex flex-row justify-between container h-svh w-full bg-slate-950 mx-auto pt-24">
-        <div className="flex items-center  gap-8 flex-col text-white mx-auto pt-48">
-          <h1 className="text-2xl md:text-3xl lg:text-5xl font-bold">
+        <div className="flex items-center gap-14 flex-col text-white mx-auto pt-48">
+          <h1 className="text-center lg:text-left px-4 lg:px-0 text-5xl font-bold">
             The Must-See App for Film Buffs.
           </h1>
           <div className="text-center text-lg">
@@ -27,22 +27,22 @@ export default function Home() {
           <div className="flex gap-10">
             <button
               type="button"
-              className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 rounded-lg text-md px-5 py-2.5 text-center me-2 mb-2"
+              className="text-white bg-gradient-to-r from-red-400 uppercase via-red-500 to-red-600 hover:ring-4 ring-white focus:outline-none  font-semibold  rounded-lg text-lg px-3 py-2 text-center me-2 mb-2"
             >
-              Learn More
+              Get Started
             </button>
             <button
               type="button"
-              className="text-red-600 bg-transparent focus:ring-4 focus:outline-none border-4 focus:ring-white border-red-600 font-medium rounded-lg text-md px-5 py-2.5 text-center me-2 mb-2"
+              className="text-red-600 bg-transparent focus:ring-4 uppercase focus:outline-none border-4 hover:ring-4 ring-white  border-red-600 font-semibold rounded-lg text-lg px-3 py-2 text-center me-2 mb-2"
             >
-              Get Started
+              Learn More
             </button>
           </div>
         </div>
 
-        <div className=" justify-self-end overflow-hidden ">
+        <div className="justify-self-end overflow-hidden ">
           <Image
-            className="hidden xl:flex "
+            className="hidden xl:flex mix-blend-luminosity border-4 border-red-950"
             src={landingImage}
             width={640}
             height={853}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -13,8 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        blue: {
+          950: "#2B2E4A",
+        },
+        red: { 100: "#E84545", 950: "#903749" },
+        purple: { 950: "#53354A" },
+      },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config

2d51cf82720aebf9651cb1fade21868aae4a21a9
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2024-04-11 14:18:56 -0400

    Create Dashboard (Dash) page

---
+++ app/dash/page.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Dash = () => {
+  return (
+    <div className="flex container bg-slate-950 mx-auto h-[calc(100svh-96px)]">
+      <div className="flex mx-auto justify-center py-8 text-5xl font-bold lg:px-0">
+        <h1 className="flex text-white">Dashboard</h1>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Dash
--- app/page.tsx
+++ app/page.tsx
@@ -12,44 +12,42 @@ export default function Home() {
   return (
     //hero
 
-    <section className="bg-slate-950 ">
-      <div className="flex flex-row justify-between container bg-slate-950 mx-auto overflow-hidden">
-        <div className="flex items-center gap-14 flex-col text-white mx-auto pt-48">
-          <h1 className="text-center lg:text-left px-4 lg:px-0 text-5xl font-bold">
-            The Must-See App for Film Buffs.
-          </h1>
-          <div className="text-center text-lg">
-            <p>Log movies you watch.</p>
-            <p>Rate and review.</p>
-            <p>Find new favorites.</p>
-            <p>All in one place.</p>
-          </div>
-          <div className="flex gap-10">
-            <button
-              type="button"
-              className="text-white bg-gradient-to-r from-red-400 uppercase via-red-500 to-red-600 hover:ring-4 ring-white focus:outline-none  font-semibold  rounded-lg text-lg px-3 py-2 text-center me-2 mb-2"
-            >
-              Get Started
-            </button>
-            <button
-              type="button"
-              className="text-red-600 bg-transparent focus:ring-4 uppercase focus:outline-none border-4 hover:ring-4 ring-white  border-red-600 font-semibold rounded-lg text-lg px-3 py-2 text-center me-2 mb-2"
-            >
-              Learn More
-            </button>
-          </div>
+    <div className="flex flex-row justify-between container bg-slate-950 mx-auto overflow-hidden">
+      <div className="flex items-center gap-14 flex-col text-white mx-auto pt-48">
+        <h1 className="text-center lg:text-left px-4  text-5xl font-bold lg:px-0">
+          The Must-See App for Film Buffs.
+        </h1>
+        <div className="text-center text-lg">
+          <p>Log movies you watch.</p>
+          <p>Rate and review.</p>
+          <p>Find new favorites.</p>
+          <p>All in one place.</p>
         </div>
-
-        <div className="justify-self-end h-[calc(100svh-96px)] overflow-hidden">
-          <Image
-            className="hidden xl:flex mix-blend-luminosity overflow-hidden"
-            src={landingImage}
-            width={640}
-            height={853}
-            alt="landing image"
-          />
+        <div className="flex gap-10">
+          <button
+            type="button"
+            className="text-white bg-gradient-to-r from-red-400 uppercase via-red-500 to-red-600 hover:ring-4 ring-white focus:outline-none  font-semibold  rounded-lg text-lg px-3 py-2 text-center me-2 mb-2"
+          >
+            Get Started
+          </button>
+          <button
+            type="button"
+            className="text-red-600 bg-transparent focus:ring-4 uppercase focus:outline-none border-4 hover:ring-4 ring-white  border-red-600 font-semibold rounded-lg text-lg px-3 py-2 text-center me-2 mb-2"
+          >
+            Learn More
+          </button>
         </div>
       </div>
-    </section>
+
+      <div className="justify-self-end h-[calc(100svh-96px)] overflow-hidden">
+        <Image
+          className="hidden xl:flex mix-blend-luminosity overflow-hidden"
+          src={landingImage}
+          width={640}
+          height={853}
+          alt="landing image"
+        />
+      </div>
+    </div>
   )
 }

6ac997222425d85a564932e04f0358631cf1a422
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2024-04-14 21:28:15 -0400

    Finish html and css for dashboard page

--- app/dash/page.tsx
+++ app/dash/page.tsx
@@ -1,26 +1,63 @@
 import React from "react"
 import Image from "next/image"
 import fireIcon from "../../public/fire.svg"
+import listIcon from "../../public/listIcon.svg"
+import accountIcon from "../../public/accountIcon.svg"
 
 const Dash = () => {
   return (
-    <div className="flex flex-col bg-slate-950 mx-auto w-full h-[calc(100svh-96px)]">
-      <div className="flex flex-col  mx-auto lg:space-y-48 py-12 lg:px-0">
-        <h1 className="flex mx-auto text-white text-5xl font-bold">
+    <div className=" bg-slate-950 mx-auto w-full min-h-[calc(100svh-96px)]">
+      <div className="flex">
+        <h1 className="flex mx-auto text-white text-5xl font-bold py-20">
           Dashboard
         </h1>
+      </div>
+      <div className="flex flex-wrap md:gap-12 justify-center">
+        <div className="overflow-hidden flex flex-col sm:w-64 w-full  bg-red-950 hover:bg-red-100 duration-100 transition-all rounded-md shadow-md  m-6">
+          <Image
+            src={fireIcon}
+            alt={"What's Hot?"}
+            className="h-52 self-center pt-10"
+          />
+          <div className="bg-black">
+            <h2 className=" text-white text-center font-semibold text-2xl px-2 py-5">
+              What's Hot
+            </h2>
+            <p className=" text-slate-500 p-3 text-center">
+              See which new movies were just released!
+            </p>
+          </div>
+        </div>
 
-        <div className="flex flex-col rounded-lg max-w-sm shadow-lg">
-          <div className="flex rounded-xl bg-red-950 flex-col w-auto align-center justify-center items-center text-center">
-            <div className="bg-red-950">
-              <Image priority src={fireIcon} alt="Image of Fire" />
-            </div>
-            <div className="flex flex-col bg-black gap-2 rounded-b-lg px-10 py-8">
-              <p className="text-white text-xl font-semibold">What's New</p>
-              <p className="text-gray-500">
-                See what hot new movies were just released!
-              </p>
-            </div>
+        <div className="overflow-hidden fill-white  flex flex-col sm:w-64 w-full bg-red-950 hover:bg-red-100 duration-100 transition-all rounded-md shadow-md  m-6">
+          <Image
+            src={listIcon}
+            alt={"What's Hot?"}
+            className="h-52 pt-5 self-center fill-white"
+          />
+          <div className="bg-black ">
+            <h2 className=" text-white text-center font-semibold text-2xl px-2 py-5">
+              My Lists
+            </h2>
+            <p className=" text-slate-500 p-3 text-center">
+              Curate personalized lists of movies
+            </p>
+          </div>
+        </div>
+
+        <div className="overflow-hidden flex flex-col  sm:w-64 w-full  bg-red-950  hover:bg-red-100 duration-100 transition-all rounded-md shadow-md  m-6">
+          <Image
+            src={accountIcon}
+            alt={"What's Hot?"}
+            className="h-52 pt-5 self-center"
+          />
+          <div className="bg-black ">
+            <h2 className=" text-white text-center font-semibold text-2xl px-2 py-5">
+              Account
+            </h2>
+            <p className=" text-slate-500 p-3 text-center">
+              Update e-mail, password, and other settings
+            </p>
           </div>
         </div>
       </div>

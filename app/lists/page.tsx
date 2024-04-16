2a3bf6de9f5ccb85e6bdd5700a8a6170a3c9c265
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2024-04-15 21:26:44 -0400

    Set background and h1 in lists

--- app/lists/page.tsx
+++ app/lists/page.tsx
@@ -2,8 +2,13 @@ import React from "react"
 
 const Lists = () => {
   return (
-    <div className="min-h-[calc(100svh-96px)] w-auto bg-slate-950">Hello</div>
+    <div className=" bg-slate-950 mx-auto w-full min-h-[calc(100svh-96px)]">
+      <div className="flex">
+        <h1 className="flex mx-auto text-white text-5xl font-bold py-20">
+          My Lists
+        </h1>
+      </div>
+    </div>
   )
 }
-
 export default Lists

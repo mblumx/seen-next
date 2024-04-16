a4e3bd061b5e82317cfbde83b943d0bb52b63f44
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2024-04-15 21:24:18 -0400

    added transform translate y to cards in dash

--- app/dash/page.tsx
+++ app/dash/page.tsx
@@ -13,7 +13,7 @@ const Dash = () => {
         </h1>
       </div>
       <div className="flex flex-wrap md:gap-12 justify-center">
-        <div className="overflow-hidden flex flex-col sm:w-64 w-full  bg-red-950 hover:bg-red-100 duration-100 transition-all rounded-md shadow-md  m-6">
+        <div className="transform hover:-translate-y-1 overflow-hidden flex flex-col sm:w-64 w-full  bg-red-950 hover:bg-red-100 duration-100 transition-all rounded-md shadow-md  m-6">
           <Image
             src={fireIcon}
             alt={"What's Hot?"}
@@ -21,7 +21,7 @@ const Dash = () => {
           />
           <div className="bg-black">
             <h2 className=" text-white text-center font-semibold text-2xl px-2 py-5">
-              What's Hot
+              What&apos;s Hot
             </h2>
             <p className=" text-slate-500 p-3 text-center">
               See which new movies were just released!
@@ -29,7 +29,7 @@ const Dash = () => {
           </div>
         </div>
 
-        <div className="overflow-hidden fill-white  flex flex-col sm:w-64 w-full bg-red-950 hover:bg-red-100 duration-100 transition-all rounded-md shadow-md  m-6">
+        <div className="transform hover:-translate-y-1 overflow-hidden fill-white  flex flex-col sm:w-64 w-full bg-red-950 hover:bg-red-100 duration-100 transition-all rounded-md shadow-md  m-6">
           <Image
             src={listIcon}
             alt={"What's Hot?"}
@@ -45,7 +45,7 @@ const Dash = () => {
           </div>
         </div>
 
-        <div className="overflow-hidden flex flex-col  sm:w-64 w-full  bg-red-950  hover:bg-red-100 duration-100 transition-all rounded-md shadow-md  m-6">
+        <div className="transform hover:-translate-y-1 overflow-hidden flex flex-col  sm:w-64 w-full  bg-red-950  hover:bg-red-100 duration-100 transition-all rounded-md shadow-md  m-6">
           <Image
             src={accountIcon}
             alt={"What's Hot?"}
